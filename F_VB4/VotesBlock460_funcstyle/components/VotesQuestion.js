b40ff5157a0bf040aef8ebc1dfcbde42d207ca14
AlexandraHessen <xl_energy@mail.ru>
2021-10-26 00:41:22 +0300

    default props

--- F_VB4/VotesBlock460_funcstyle/components/VotesQuestion.js
+++ F_VB4/VotesBlock460_funcstyle/components/VotesQuestion.js
@@ -13,6 +13,20 @@ VotesQuestion.propTypes = {
 
 export default VotesQuestion;
 
+// class VotesQuestion extends React.Component {
+
+//   static propTypes = {
+//     question: PropTypes.string.isRequired,
+//   };
+  
+//   render() {
+//     return <div className='VotesQuestion'>{this.props.question}</div>;
+//   }
+
+// }
+
+
+
 // если propTypes не нужны, можно короче (экспортируется безымянная функция):
 /*
 import React from 'react';
@@ -31,4 +45,14 @@ import React from 'react';
 import './VotesQuestion.css';
 
 export default props => <div className='VotesQuestion'>{props.question}</div>;
+
+как в данном случае родитель понял что это имеено этот кодд ведь он у нас без имени?
+
+не имеет значения как мы его тут в это модуле назовем или не назовем
+
+главное что родительский модуль по пути ('./VotesQuestion') из это получает код а имя уже в родительском модум присваивает сам
+
+в общем код идет не по имени а по размещению('./VotesQuestion')
+это касается DEFAULT!!!
 */
+
